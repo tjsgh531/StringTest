d08bc095acc8d973ce5b3684fbb7cbae4932bc2d
Guest <guest-ewpwbg@h26632.sgb.skir.kr>
2020-12-31 23:07:50 +0900

    test

--- main.js
+++ main.js
@@ -7,15 +7,20 @@ const target = "sit";
 let temp;
 const searchStrlen = target.length;
 
-let resultArr = [];
+let startIndex = [];
 /*시작 인덱스 찾는 코드*/
 for(let i  = 0 ; i < testString.length; i++){
     if(testStringArr[i].toString() == "s"){
         temp = testString.substring(i,i+searchStrlen);
         if(temp == target){
-            resultArr.push(i);
+            startIndex.push(i);
         }
     }
 }
 
 console.log(resultArr);
+
+/*target만 붙이고 나머지는 다 떨어진 문자열 배열 만들기 */
+for(let i = 0 ; i < startIndex.length ; i++){
+    testStringArr.splice(startIndex[i],searchStrlen)
+}
